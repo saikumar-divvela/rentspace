6046587dc9ad984bf04a10938627029e7cdde6aa
saikumar divvela <saikumar.divvela@gmail.com>
2016-05-09 22:54:28 +0900

    updated code

--- code/rentspace/urls.py
+++ code/rentspace/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('users.urls')),
+    #url(r'^', include(router.urls)),
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
