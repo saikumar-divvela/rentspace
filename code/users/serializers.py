3a63869485b3877fa744321441a93f744aa40433
saikumar divvela <saikumar.divvela@gmail.com>
2016-05-18 17:27:23 +0900

    updated api

--- code/users/serializers.py
+++ code/users/serializers.py
@@ -1,19 +1,31 @@
 from rest_framework import serializers
 from users.models import User
 from users.models import Post
+from users.models import PostAttributes
 
+
+class PostAttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostAttributes
+        #fields = ( 'emailid','phone_number','first_name','last_name','posts')
+        #read_only_fields = ('id',)
 
 
 class PostSerializer(serializers.ModelSerializer):
+    attributes = PostAttributeSerializer(read_only=True,many=True)
     class Meta:
         model = Post
         #fields = ('id','description','user')
-        read_only_fields = ('user',)
+        #read_only_fields = ('id',)         
+        #exclude=('user',)
 
 class UserSerializer(serializers.ModelSerializer):
-    #posts = PostSerializer(many=True)
+    posts = PostSerializer(read_only=True,many=True)
     class Meta:
         model = User
         #fields = ( 'emailid','phone_number','first_name','last_name','posts')
-        read_only_fields = ('id',)
+        #read_only_fields = ('id',)
+        #exclude=('id',)
+
+
 
--- code/users/views.py
+++ code/users/views.py
@@ -1,5 +1,5 @@
-from users.models import User,Post
-from users.serializers import UserSerializer,PostSerializer
+from users.models import User,Post,PostAttributes
+from users.serializers import UserSerializer,PostSerializer,PostAttributeSerializer
 
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
@@ -106,20 +106,25 @@ def user_status(request,pk):
          return JSONResponse(serializer.data)  
          #return Response(serializer.data) 
 
+
+
 @permission_classes((permissions.AllowAny,))
 class PostList(APIView):
     def get(self,request,user_pk,format=None):
-        posts = Post.objects.all();
+        user =UserDetail().get_object(user_pk)
+        posts = Post.objects.filter(user__id=user_pk);
         serializer = PostSerializer(posts,many=True)
-        #print serializer.data
         return Response(serializer.data)
 
     def post(self,request,user_pk,format=None):    
+        user =UserDetail().get_object(user_pk)
         request.data["user"]= user_pk
-        print request.data
+        print request.data["attributes"]
         serializer = PostSerializer(data=request.data)
+        print serializer
         if serializer.is_valid():
             serializer.save()
+            add_post_attributes(request.data["attributes"],serializer.data["id"])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -149,6 +154,7 @@ class PostDetail(APIView):
          serializer = PostSerializer(post,data=request.data)     
          if serializer.is_valid():
             serializer.save()
+            update_post_attributes(request.data["attributes"],serializer.data["id"])
             return Response(serializer.data)   
          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -156,6 +162,80 @@ class PostDetail(APIView):
         post = self.get_object(pk)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+def add_post_attributes(attributelist,post_id):
+    for attribute in attributelist:
+        attr_data = {}
+        attr_data["post"]=post_id
+        for name,value in attribute.items():    
+            attr_data["name"]=name
+            attr_data["value"]=value
+            attr_serializer = PostAttributeSerializer(data=attr_data)
+            if attr_serializer.is_valid():
+                attr_serializer.save()
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def update_post_attributes(attributelist,post_id):
+    postattributes = PostAttributes.objects.filter(post__id=post_id);
+    for attribute in postattributes:
+        attribute.delete()
+    add_post_attributes(attributelist,post_id)
+
+@csrf_exempt
+def post_status(request,user_pk,pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        raise Http404
+    
+    if request.method == "GET":
+        output = {}
+        output["status"] = post.status
+        output["is_verified"] = post.is_verified
+        output["is_active"] = post.is_active
+                
+        serializer = PostSerializer(post)
+        return JSONResponse(output)
+
+
+    elif request.method == "PUT":
+         data = JSONParser().parse(request)
+         for key,value in data.items():
+            post[key]=value    
+         post.save()
+
+         serializer = PostSerializer(post)
+         return JSONResponse(serializer.data)  
+         #return Response(serializer.data) 
+
+
+@permission_classes((permissions.AllowAny,))
+class PostAttributeList(APIView):
+    def get(self,request,user_pk,post_pk,format=None):
+        postattributes = PostAttributes.objects.filter(post__id=post_pk)
+        serializer = PostAttributeSerializer(postattributes,many=True)
+        return Response(serializer.data)
+
+    def post(self,request,user_pk,post_pk,format=None):    
+        print request.data
+        update_post_attributes(request.data["attributes"],post_pk)
+        postattributes = PostAttributes.objects.filter(post__id=post_pk)
+        serializer = PostAttributeSerializer(postattributes,many=True)
+        return Response(serializer.data)
+
+    def put(self,request,user_pk,post_pk,format=None):    
+        print request.data
+        update_post_attributes(request.data["attributes"],post_pk)
+        postattributes = PostAttributes.objects.filter(post__id=post_pk)
+        serializer = PostAttributeSerializer(postattributes,many=True)
+        return Response(serializer.data)
+
+    
+
 
 '''
 @permission_classes((permissions.AllowAny,))
