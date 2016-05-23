2e3df1b1e56179165636a5aebd282e124cd5144d
saikumar divvela <saikumar.divvela@gmail.com>
2016-05-23 21:49:33 +0900

    added rest api for images

--- code/users/serializers.py
+++ code/users/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from users.models import User
 from users.models import Post
 from users.models import PostAttributes
+from users.models import Comment
+from users.models import Image
 
 
 class PostAttributeSerializer(serializers.ModelSerializer):
@@ -28,4 +30,22 @@ class UserSerializer(serializers.ModelSerializer):
         #exclude=('id',)
 
 
+class ImageSerializer(serializers.ModelSerializer):
+    filecontent = serializers.SerializerMethodField()
+    class Meta:
+        model = Image
+        #fields = ( 'emailid','phone_number','first_name','last_name','posts')
+        #read_only_fields = ('filepath','filetype',)
+        #exclude=('filepath','filetype',)
+
+    def get_filecontent(self, obj):
+        #return (now() - obj.date_joined).days
+        return  obj.filepath
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        #fields = ( 'emailid','phone_number','first_name','last_name','posts')
+        #read_only_fields = ('id',)
 
