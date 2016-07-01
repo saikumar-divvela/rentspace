f0a998677614bfea6ac4026c03971828d39aa76b
saikumar divvela <sai.divvela@samsung.com>
2016-07-01 22:05:58 +0900

    added test client to test rest api

--- rentspace/config/settings.py
+++ rentspace/config/settings.py
@@ -157,6 +157,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
     )
 }
 
