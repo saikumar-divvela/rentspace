ef4a265bb0a3cb7c3b0d97e3d2fa19ad48838449
saikumar divvela <sai.divvela@samsung.com>
2016-09-02 21:47:34 +0900

    added search filter and user queries to admin page

--- rentspace/apps/myadmin/models.py
+++ rentspace/apps/myadmin/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class  Postbox(models.Model):
+    name    = models.CharField(max_length=50)
+    email   = models.EmailField(max_length=50,db_index=True)
+    phone_number   = models.CharField(max_length=10,db_index=True)
+    subject = models.CharField(max_length=100)
+    message = models.CharField(max_length=255)
+    sent_date = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table="postbox"
